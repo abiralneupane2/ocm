77494ad60acc50b0dfa611b787d96142f8031a7c
Abiral <abiralneupane2@gmail.com>
2021-08-21 21:07:49 +0545

    added templates

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('profile/', views.profile, name='profile'),
+    path('browse/', views.browse, name='browse'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render, HttpResponse, redirect
+from django.urls import reverse
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>hello</h1>")
+    return render(request, 'index.html')
+
+def profile(request):
+    if request.user.is_authenticated:
+        return render(request, 'profile.html')
+    else:
+        return redirect(reverse('index'))
+
+
+def browse(request):
+    
+    return render(request, 'browse.html')
